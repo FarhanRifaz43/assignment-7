5ba4ba79c838997aca9fe8af06a6afff984eef03
Farhan Rifaz <farhanrifaz@Farhans-MacBook-Pro.local>
2023-09-16 11:00:28 +0600

    fixed JSON

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
 
   useEffect(() => {
-    fetch('../public/courses.json')
+    fetch('courses.json')
       .then(res => res.json())
       .then(data => setCourses(data))
   }, [])
